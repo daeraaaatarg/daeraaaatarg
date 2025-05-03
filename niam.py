fe124523fab014aa7c5c48986937e1c5a8d23c91
ACER <kutova.anastasia00@gmail.com>
2025-05-03 13:24:39 +0300

    add calculator

--- niam.py
+++ niam.py
@@ -1,1 +1,19 @@
 print("hello world")
+
+a = float(input("Перше число: "))
+b = float(input("Друге число: "))
+op = input("Операція (+, -, *, /): ")
+
+if op == "+":
+    print("Результат:", a + b)
+elif op == "-":
+    print("Результат:", a - b)
+elif op == "*":
+    print("Результат:", a * b)
+elif op == "/":
+    if b != 0:
+        print("Результат:", a / b)
+    else:
+        print("Помилка: ділення на нуль!")
+else:
+    print("Невідома операція")
